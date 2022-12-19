70edf0671d267a99ab9b7d73777b8cff6a1fa134
elbolaestevez <elbolaestevez@gmail.com>
2022-12-19 18:30:24 -0300

    updates

---
+++ api/utils/scraper.js
@@ -0,0 +1,72 @@
+const puppeteer = require("puppeteer");
+const scraperByMovie = async (title = "harry") => {
+  //   console.log("hola", title);
+  let movieUrl = `https://www.sensacine.com/buscar/?q=${title}`;
+  let browser = await puppeteer.launch({
+    headless: false,
+    // args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  let page = await browser.newPage();
+  //   await page.type('#mytextarea', 'Hello');
+
+  await page.goto(movieUrl);
+
+  // Esto solo para el navegador porque hay cookie, cuando estas en handless true no es necesario.
+  //await page.waitForSelector("#didomi-notice-agree-button");
+  //   await page.click("#didomi-notice-agree-button");
+  const links = await page.evaluate(() => {
+    const elements = document.querySelectorAll(".rating-holder a");
+    console.log("elements", elements);
+    let links = [];
+    for (let element of elements) {
+      links.push(element.href);
+    }
+    return links;
+  });
+  console.log("links", links);
+
+  const match = links.filter((element) =>
+    element.includes("criticas-espectadores")
+  );
+  console.log("match1", match);
+  //   (enlaces = []), (busqueda = []);
+
+  let busqueda = [];
+  for (let enlace of match) {
+    console.log("enlace", enlace);
+    await page.goto(enlace);
+    //  await page.screenshot({ path: "foto.jpg" });
+    //  await page.click("#didomi-notice-agree-button");
+
+    await page.keyboard.press("Enter");
+
+    const dataend = await page.evaluate(() => {
+      const tmp = {};
+      tmp.titulo = document.querySelector("a.xXx.titlebar-link").text;
+      tmp.innerUserOfReview = document.querySelectorAll(
+        "div.review-card-aside > div > div > div > a"
+      )[0].text;
+      tmp.innerUserOfReview2 = document.querySelectorAll(
+        "div.review-card-aside > div > div > div > a"
+      )[1].text;
+      tmp.innerTextOfReview = document.querySelectorAll(
+        "div.review-card-review-holder > div.content-txt.review-card-content"
+      )[0].textContent;
+      tmp.innerTextOfReview2 = document.querySelectorAll(
+        "div.review-card-review-holder > div.content-txt.review-card-content"
+      )[1].textContent;
+
+      return tmp;
+    });
+    busqueda.push(dataend);
+    console.log("dataend", dataend);
+  }
+  await browser.close();
+};
+
+scraperByMovie();
+
+// const ScraperTools = {
+//   scraperByMovie,
+// };
+module.exports = { scraperByMovie };
--- src/state/user.js
+++ src/state/user.js
@@ -1,9 +1,26 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: {},
+  id: null,
+  nombre: null,
+  email: null,
+  tipo: false,
+  superAdmin: false,
 };
-export const setUser = createAction("SET_USER");
-export const reducer = createReducer(initialState, {
-  [setUser]: (state, action) => (state = { value: action.payload }),
+
+export const userLogin = createAction("USER_LOGIN");
+export const userLogOut = createAction("USER_LOGOUT");
+
+const userReducer = createReducer(initialState, {
+  [userLogin]: (state, action) =>
+    (state = {
+      id: action.payload.id,
+      nombre: action.payload.nombre,
+      email: action.payload.email,
+      tipo: action.payload.tipo,
+      superAdmin: action.payload.superAdmin,
+    }),
+  [userLogOut]: (state, action) => (state = {}),
 });
+
+export default userReducer;
